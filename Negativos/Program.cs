bbc336c279c41775ebc98a921fc77471a9a0b03c
Tony <antony.branquinho@gmail.com>
2023-12-06 09:25:09 -0300

    De primeira

---
+++ Negativos/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negativos {
+    class Program {
+        static void Main(string[] args) {
+
+            int n, i;
+
+            Console.Write("Quantos numeros voce vai digitar? ");
+            n = int.Parse(Console.ReadLine());
+
+            double[] vet = new double[n];
+
+            for (i = 0; i < n; i++) {
+                Console.Write("Digite um numero: ");
+                vet[i] = double.Parse(Console.ReadLine()); 
+            }
+
+            Console.WriteLine("NUMEROS NEGATIVOS:");
+            for (i = 0; i < n; i++) {
+                if (vet[i] < 0) {
+                    Console.WriteLine(vet[i]);
+                }
+            }
+
+        }
+    }
+}
